1bb454bfd1729fdf54d675a921b4b32804a3667a
MauroBorzi <mauroborzi91@gmail.com>
2025-11-03 14:43:29 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+class Movie
+{
+  public $titolo;
+  public $autore;
+  public $anno;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ex-php8-oop-movie</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
