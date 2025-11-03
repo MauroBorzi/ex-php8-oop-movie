b177697fb4fd445eb026ae490ca0d8608a734e00
MauroBorzi <mauroborzi91@gmail.com>
2025-11-03 15:19:21 +0100

    Refactor Movie class to use an array of Genre objects and implement RatingTrait for rating functionality

--- index.php
+++ index.php
@@ -11,52 +11,64 @@ class Genre
   }
 }
 
+trait RatingTrait
+{
+  public $rating;
+
+  public function setRating($valore)
+  {
+    if ($valore < 0 || $valore > 10) {
+      throw new Exception("Il voto deve essere compreso tra 0 e 10");
+    }
+    $this->rating = $valore;
+  }
+
+  public function getRating()
+  {
+    return $this->rating ?? "Nessun voto assegnato";
+  }
+}
+
 class Movie
 {
+  use RatingTrait;
+
   public $titolo;
   public $autore;
   public $anno;
-  public $genere;
+  public $generi = [];
 
-  function __construct($_titolo, $_autore, $_anno, Genre $_genere)
+  function __construct($_titolo, $_autore, $_anno, array $_generi)
   {
     $this->titolo = $_titolo;
     $this->autore = $_autore;
     $this->anno = $_anno;
-    $this->genere = $_genere;
+    $this->generi = $_generi;
   }
 
   public function getTitle()
   {
     return $this->titolo;
   }
+
+  public function getGeneri()
+  {
+    $nomiGeneri = [];
+    foreach ($this->generi as $genere) {
+      $nomiGeneri[] = "{$genere->genere} ({$genere->sottogenere})";
+    }
+    return implode(", ", $nomiGeneri);
+  }
 }
 
-$genre = new Genre("Azione", "Ammazzo tutti");
+$genre1 = new Genre("Azione", "Sparatorie");
+$genre2 = new Genre("Thriller", "Vendetta");
 
-$movie1 = new Movie("John Wick", "ken'n so", 2014, $genre);
-$movie2 = new Movie("John Wick 2", "ken'n so2", 2017, $genre);
-var_dump($movie1);
-echo "<br>";
-echo $movie1->getTitle();
-echo "<br>";
-var_dump($movie2);
-echo "<br>";
-echo $movie2->getTitle();
+$movie1 = new Movie("John Wick", "Chad Stahelski", 2014, [$genre1, $genre2]);
+$movie2 = new Movie("John Wick 2", "Chad Stahelski", 2017, [$genre1]);
 
-?>
+$movie1->setRating(9);
+$movie2->setRating(8.5);
 
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>ex-php8-oop-movie</title>
-</head>
-
-<body>
-
-</body>
-
-</html>
+echo "<strong>{$movie1->getTitle()}</strong> - Generi: {$movie1->getGeneri()} - Rating: {$movie1->getRating()}<br>";
+echo "<strong>{$movie2->getTitle()}</strong> - Generi: {$movie2->getGeneri()} - Rating: {$movie2->getRating()}<br>";
