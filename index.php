968da66637e9ee97b9b044775d2965dc748910ba
MauroBorzi <mauroborzi91@gmail.com>
2025-11-03 15:02:46 +0100

    Implement Movie class with constructor and methods for title retrieval

--- index.php
+++ index.php
@@ -4,7 +4,29 @@ class Movie
   public $titolo;
   public $autore;
   public $anno;
+
+  function __construct($_titolo, $_autore, $_anno)
+  {
+    $this->titolo = $_titolo;
+    $this->autore = $_autore;
+    $this->anno = $_anno;
+  }
+
+  public function getTitle()
+  {
+    return $this->titolo;
+  }
 }
+$movie1 = new Movie("John Wick", "ken'n so", 2014);
+$movie2 = new Movie("John Wick 2", "ken'n so2", 2017);
+var_dump($movie1);
+echo "<br>";
+echo $movie1->getTitle();
+echo "<br>";
+var_dump($movie2);
+echo "<br>";
+echo $movie2->getTitle();
+
 ?>
 
 <!DOCTYPE html>
