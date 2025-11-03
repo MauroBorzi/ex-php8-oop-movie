6f23b7d77fe7feb82adde9f6cc876a1f7142d267
MauroBorzi <mauroborzi91@gmail.com>
2025-11-03 15:11:19 +0100

    Add Genre class and update Movie constructor to include genre

--- index.php
+++ index.php
@@ -1,15 +1,29 @@
 <?php
+class Genre
+{
+  public $genere;
+  public $sottogenere;
+
+  function __construct($_genere, $_sottogenere)
+  {
+    $this->genere = $_genere;
+    $this->sottogenere = $_sottogenere;
+  }
+}
+
 class Movie
 {
   public $titolo;
   public $autore;
   public $anno;
+  public $genere;
 
-  function __construct($_titolo, $_autore, $_anno)
+  function __construct($_titolo, $_autore, $_anno, Genre $_genere)
   {
     $this->titolo = $_titolo;
     $this->autore = $_autore;
     $this->anno = $_anno;
+    $this->genere = $_genere;
   }
 
   public function getTitle()
@@ -17,8 +31,11 @@ class Movie
     return $this->titolo;
   }
 }
-$movie1 = new Movie("John Wick", "ken'n so", 2014);
-$movie2 = new Movie("John Wick 2", "ken'n so2", 2017);
+
+$genre = new Genre("Azione", "Ammazzo tutti");
+
+$movie1 = new Movie("John Wick", "ken'n so", 2014, $genre);
+$movie2 = new Movie("John Wick 2", "ken'n so2", 2017, $genre);
 var_dump($movie1);
 echo "<br>";
 echo $movie1->getTitle();
